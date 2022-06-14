8e4b4dfa0c316ed091fd15e64e2e55c458ef25d8
MontenegroMarcos <marcosmontenegro434@gmail.com>
2022-06-14 13:26:40 -0300

    Se agrega Test unitario Hash, revisar metodo Remove

---
+++ Tests/HashTest/TestHashMethods.java
@@ -0,0 +1,41 @@
+package HashTest;
+
+import TADs.MyHashTableIMPL;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestHashMethods {
+
+    MyHashTableIMPL<Integer,String> miTablaHash = new MyHashTableIMPL<>();
+    MyHashTableIMPL<Integer,String> miTablaHash2 = new MyHashTableIMPL<>();
+    MyHashTableIMPL<Integer,Object> miTablaHash3 = new MyHashTableIMPL<>();
+
+    @Test
+    public void testPut(){
+        miTablaHash.put(5,"Carlo");
+        miTablaHash.put(6,"Rober");
+        assertTrue("La tabla no funciona",miTablaHash.size() != 0 && miTablaHash.get(5).equals("Carlo"));
+    }
+
+    @Test
+    public void testRemove(){
+        miTablaHash3.put(5,"Carlo");
+        miTablaHash3.put(6,"Rober");
+        miTablaHash3.remove(5);
+      //assertNull("El metodo remover no funca", miTablaHash3);
+    }
+
+    @Test
+    public void testGet(){
+        miTablaHash2.put(4,"Gette");
+        assertTrue("El get no funciona", miTablaHash2.get(4).equals("Gette"));
+    }
+
+    @Test
+    public void testSize(){
+        miTablaHash.put(20,"Fede");
+        miTablaHash.put(10,"Teo");
+        assertTrue("No funciona el Size", miTablaHash.size() == 2);
+    }
+}
